1a8e005672ac1f838e0f416a14a8268c959128a7
Administrator <Administrator@DESKTOP-VFV26N6>
2024-11-28 10:03:35 +0800

    设定某些属性为必填

--- Data.Handler/Models/RuleModel.cs
+++ Data.Handler/Models/RuleModel.cs
@@ -9,7 +9,7 @@ namespace Data.Handler.Models
     public class RuleModel<T>
     {
         public required bool IsActive { get; set; } //规则是否激活
-        public string RuleName { get; set; } = string.Empty; //规则名称
+        public required string RuleName { get; set; } //规则名称
         public required RuleType RuleType { get; set; } //规则类型
         public Func<T[], (bool Res, T[] DataWashed, T? ErrorItem)>? WashingRule { get; set; } //清洗方法
         public Func<List<T>, (bool Res, T? ErrorItem)>? CheckingRule { get; set; } //检测方法
